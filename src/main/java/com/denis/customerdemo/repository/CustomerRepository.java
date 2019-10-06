51ae93570dd478332fb74340eb0ac6da2a1e9df0
denisshchurin <denisshchurin@gmail.com>
2019-10-06 13:01:32 +0300

    add Interface CustomerRepository

---
+++ src/main/java/com/denis/customerdemo/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.denis.customerdemo.repository;
+
+import com.denis.customerdemo.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository interface for {@link Customer} class.
+ *
+ * @author Denis Shchurin
+ * @version 1.0
+ */
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {t
+
+}
